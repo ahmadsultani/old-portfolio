45686a86f1e8e466863584de1fc627fb07420885
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-02-27 07:25:50 +0800

    js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $('.navbar-item').on('click', function(e) {
     var destination = $(this).attr('href');
     var element = $(destination);
     $('body').animate({
-        scrollTop: element.offset().top - 50
+        scrollTop: element.offset().top + 10
     });
     e.preventDefault();
 });
