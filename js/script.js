93ace5bb3c1119a6470db2b1e26b9c19862994b2
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-09-16 22:55:59 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
 $('.navbar-item').on('click', function(e) {
     var destination = $(this).attr('href');
     var element = $(destination);
-    $('body').animate({
-        scrollTop: element.offset().top + 10
+    $('html, body').animate({
+        scrollTop: element.offset().top
     });
     e.preventDefault();
 });
 
 const img = document.querySelector('.img');
-img.setAttribute('draggable',false)
-
-//parallax
-$(halo).scroll(function() {
-    var wScroll = $(this).scrollTop();
-    $('.jumbotron ').css({
-        'transform': 'translate(0px, ' + wScroll + 'px)'
-    });
-})
+img.setAttribute('draggable',false);
